bb9d14d7fd2c74d148d5fd2bab78111f08386deb
Thomas Coats <github@voodoolabs.net>
2018-11-10 21:35:35 +1300

    updated documentation and removed unused functions

--- README.md
+++ README.md
@@ -1,47 +1,75 @@
-Dependency Injection
---------------------
+# Dependency Injection
 
-config.coffee
-```coffeescript
-inject = require 'pminject'
+pod.js
+```javascript
+const inject = require('injectinto')
 
-inject.bind templaterenderer: require 'nun'
-inject.bind contenttypes: [
-    require './contenttypes/text'
-    require './contenttypes/image'
-]
+inject('pod', () => {
+  const db = inject.one('db')
+
+  db.key = 'value'
+})
 ```
 
-cms.coffee
-```coffeescript 
-inject = require 'pminject'
+index.js
+```javascript
+const inject = require('injectinto')
 
-contenttypes = inject.many 'contenttypes'
-templaterenderer = inject.one 'templaterenderer'
+const db = {}
+inject('db', db)
 
-for contenttype in contenttypes
-    templaterenderer.render contenttype
+for (let pod of inject.many('pod')) pod()
 ```
 
-
-What is the problem?
---------------------
-
+## What is the problem?
 I'd like to build systems that can easily be extended, enhanced, or replaced.
 
+## How injectinto solves this problem
+1. One or more objects are bound to string keys, for example 'db'
+2. Other parts of the code can request objects by providing in the key, for example 'db'
 
-How injectinto solves this problem
-----------------------------------
+# API
+* [<code>inject(key, value)</code>](#bind)
+* [<code>inject.bind(key, value)</code>](#bind)
+* [<code>inject.unbind(key, value)</code>](#unbind)
+* [<code>inject.one(key)</code>](#one)
+* [<code>inject.oneornone(key)</code>](#oneornone)
+* [<code>inject.many(key)</code>](#many)
+* [<code>inject.clear(key)</code>](#clear)
+* [<code>inject.reset()</code>](#reset)
 
-1. Extension points are defined through strings, for example 'templateengine'
-2. In configuration these extension points are bound to actual objects
-3. A module asks for an object via it's string, for example 'templateengine'
-4. Some modules can expect an array of objects
+<a name="bind"></a>
+## `inject(key, value)`
+## `inject.bind(key, value)`
 
+Register a value to a string key for retrieval later. Multiple calls will register multiple objects and be accessed via [<code>inject.many(key)</code>](#many)
 
-Goals
-------
+<a name="unbind"></a>
+## `inject.unbind(key, value)`
 
-1. Simple
-2. Work with async
-3. Allow multiple registrations
+Remove a registered value for a key. Does not error if value is not found.
+
+<a name="one"></a>
+## `inject.one(key)`
+
+Return a single registered value for a given key. Will error if no value is found or if more than one value is found.
+
+<a name="oneornone"></a>
+## `inject.oneornone(key)`
+
+Return a single registered value or null for a given key. Will error if more than one value is found.
+
+<a name="many"></a>
+## `inject.many(key)`
+
+Return all registered values for a given key. Will return an empty array if no values are registered.
+
+<a name="clear"></a>
+## `inject.clear(key)`
+
+Clears all registered values for a given key.
+
+<a name="reset"></a>
+## `inject.reset()`
+
+Clears all registered values for all keys.
--- inject.js
+++ inject.js
@@ -1,95 +1,47 @@
 const inject = () => {
   let bindings = {}
+  const unbind = (key, value) => {
+    if (bindings[key] == null) return
+    const items = bindings[key]
+    const index = items.indexOf(value)
+    if (index !== -1) items.splice(index, 1)
+  }
   return {
-    bind: (key, item) => {
-      if (typeof(key) === 'object') {
-        for (let k in key) res.bind(k, key[k])
-        return {
-          off: () => {
-            for (let k in key) {
-              if (bindings[key] == null) continue
-              const items = bindings[key]
-              const index = items.indexOf(key[k])
-              if (index !== -1) items.splice(index, 1)
-            }
-          }
-        }
-      }
-      if (bindings[key] == null) bindings[key] = []
-      if (Array.isArray(item)) {
-        bindings[key] = bindings[key].concat(item)
-        return {
-          off: () => {
-            for (let i of item) {
-              if (bindings[key] == null) continue
-              const items = bindings[key]
-              const index = items.indexOf(i)
-              if (index !== -1) items.splice(index, 1)
-            }
-          }
-        }
-      }
-
-      bindings[key].push(item)
-      return {
-        off: () => {
-          if (bindings[key] == null) return
-          const items = bindings[key]
-          const index = items.indexOf(item)
-          if (index !== -1) items.splice(index, 1)
-        }
-      }
+    bind: (key, value) => {
+      bindings[key].push(value)
+      return { off: () => unbind(key, value) }
     },
-
+    unbind: unbind,
     one: (key) => {
       if (bindings[key] == null) throw new Error(`${key} not found`)
       const items = bindings[key]
       if (items.length > 1) throw new Error(`${key} too many bound`)
       return items[0]
     },
-
     oneornone: (key) => {
       if (bindings[key] == null) return null
       const items = bindings[key]
       if (items.length > 1) throw new Error(`${key} too many bound`)
       return items[0]
     },
-
-    first: (key) => {
-      if (bindings[key] == null) throw new Error(`${key} not found`)
-      return bindings[key][0]
-    },
-
-    firstornone: (key) => {
-      if (bindings[key] == null) return null
-      return bindings[key][0]
-    },
-
     many: (key) => {
       if (bindings[key] == null) return []
       return bindings[key]
     },
-
-    clear: (key) => {
-      delete bindings[key]
-    },
-
-    clearAll: () => {
-      bindings = {}
-    }
+    clear: (key) => delete bindings[key],
+    reset: () => bindings = {}
   }
 }
 
-let _inject = inject()
-module.exports = (key, item) => {
+const _inject = inject()
+module.exports = (key, value) => {
   if (key == null) return inject()
-  return _inject.bind(key, item)
+  return _inject.bind(key, value)
 }
-module.exports.bind = (key, item) => _inject.bind(key, item)
+module.exports.bind = (key, value) => _inject.bind(key, value)
+module.exports.unbind = (key, value) => _inject.unbind(key, value)
 module.exports.one = (key) => _inject.one(key)
 module.exports.oneornone = (key) => _inject.oneornone(key)
-module.exports.first = (key) => _inject.first(key)
-module.exports.firstornone = (key) => _inject.firstornone(key)
 module.exports.many = (key) => _inject.many(key)
 module.exports.clear = (key) => _inject.clear(key)
-module.exports.clearAll = () => _inject.clearAll()
+module.exports.reset = () => _inject.reset()
